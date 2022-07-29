d5d0dd750024cc2aeb9886c36655225922328d34
kerim <krmksmsite@gmail.com>
2022-07-29 16:34:56 +0300

    Project Completed

---
+++ Expanding Cards/README.md
@@ -0,0 +1,13 @@
+```
+
+In this project, I made a Expanding Cards
+
+You can see the card wider when you click it.
+
+```
+You can see the project live here: [Netlify Linki](https://expanding-cards-site.netlify.app)
+
+## Image
+
+![cards](https://user-images.githubusercontent.com/96295567/181770479-88480f0e-57ab-4d0b-8a3b-33169fc41657.png)
+
---
+++ Expanding Cards/script.js
@@ -0,0 +1,15 @@
+
+let panels = document.querySelectorAll(".panel")
+
+panels.forEach((item) => {
+        item.addEventListener("click", () => {
+            removeActiveClass()
+            item.classList.add("active")
+        })
+})
+
+function removeActiveClass() {
+    panels.forEach((item) => {
+        item.classList.remove("active")
+    })
+}
